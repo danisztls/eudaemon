02cce77130b3ccf69270ff5c57a6911af7f2a409
Daniel Souza <me@posix.dev.br>
2021-11-13 10:40:43 -0300

    Configure README on setup
    Rename file to '.rst'.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     version=1.0,
     name='eudaemon',
     description='Monitor activity and help user to be conscious about it and to take positive action.',
+    long_description=open('README.rst', 'rt').read(),
     author='Daniel Souza',
     author_email='me@posix.dev.br',
     url='',
