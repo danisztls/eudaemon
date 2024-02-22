cb25d985325fd6b851bab9d5c1662a3810b4d9de
Daniel Souza <me@posix.dev.br>
2024-02-22 07:43:45 -0300

    enable concurrency

--- eudaemon/__init__.py
+++ eudaemon/__init__.py
@@ -4,12 +4,18 @@
 Eudaemon: Monitor and log activity, improve awareness and empower user to improve it's habits.
 """
 
+import threading
 from .idleness import main as idleness_monitor
 from .brightness import main as brightness_monitor
 
+
 def main():
-    idleness_monitor()
-    brightness_monitor()
+    idleness = threading.Thread(target=idleness_monitor)
+    brightness = threading.Thread(target=brightness_monitor)
+    idleness.start()
+    brightness.start()
+    idleness.join()
+    brightness.join()
 
 
 if __name__ == "__main__":
